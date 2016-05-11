54af0d62c4a2827073a973002f67a50e26fc2f66
Can Bülbül <alicanblbl@gmail.com>
2016-05-12 00:01:33 +0300

    backend

--- service.py
+++ service.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 from flask import Flask, request, render_template
 from werkzeug.debug import DebuggedApplication
 from flup.server.fcgi import WSGIServer
+from redis import StrictRedis
+from datetime import datetime, timedelta
+from calendar import monthrange
 
 #from email.MIMEMultipart import MIMEMultipart
 #from email.MIMEBase import MIMEBase
@@ -12,12 +16,53 @@ from flup.server.fcgi import WSGIServer
 app = Flask(__name__)
 app.debug = True
 app.wsgi_app = DebuggedApplication(app.wsgi_app, True)
+rdb = StrictRedis('localhost',6379,3)
 
 censor = lambda s: "%s%s%s"%(s[0], (len(s)-2)*'*',s[-1])
 
+def make_week_from(date):
+    start = date - timedelta(days=date.weekday())
+    week = map(lambda n: datetime.strftime( start + timedelta(days=n) , '%d-%m-%Y' ), range(5))
+    return week
+
+def getweek(day):
+    lkey = 'weekly.lectures.my.%s'%day
+    if(rdb.llen(lkey)==0): 
+        print lkey
+        map( lambda _: rdb.lpush(lkey, 'none') , range(8))
+    return rdb.lrange(lkey,0,8)
+
 @app.route('/weekly/')
 def home():
-    return render_template('index.html')
+    week  = make_week_from(datetime.today())
+    lweek = zip( *map(getweek, week) )
+    courses = map(lambda s: s.split(':'), rdb.smembers('weekly.courses.my'))
+    hours = ['8:40','9:40','10:40','11:40','13:40','14:40','15:40','16:40']
+    return render_template('index.html', lectures=lweek, courses=courses, hours=hours)
+
+@app.route('/weekly/course/all/')
+def get_courses():
+    courses = map(lambda s: s.split(':'), rdb.smembers('weekly.courses.my'))
+    return jsonify(courses=courses)
+
+@app.route('/weekly/course/add/',methods=['POST'])
+def add_course():
+    name = request.form['name']
+    lecturer = request.form['lecturer']
+
+    rdb.sadd('weekly.courses.my','%s:%s'%(name,lecturer))
+    return jsonify(message='ok',course=name)
+
+@app.route('/weekly/lecture/add/',methods=['POST'])
+def add_lecture():
+    course = request.form['name']
+    day = request.form['day']
+    hour = request.form['hour']
+    lkey = 'weekly.lectures.my.%s'%day
+    if(rdb.llen(lkey)==0):
+        map( rdb.lpush(lkey, 'none'), range(5))
+    rdb.lset(lkey, int(hour), course)
+    return jsonify(message='ok',course=course)
 
 if __name__ == '__main__':
     app.run(host="localhost",port=80,debug=True)
